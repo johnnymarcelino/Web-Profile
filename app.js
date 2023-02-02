d6c6cd1e8c4ba090f8b26d1ca8fd55f498e72b20
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-02-01 23:03:06 -0300

    feat: page home completed

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const sections = document.querySelectorAll('.section');
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+/* const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
@@ -33,4 +47,4 @@ function PageTransitions(){
     })
 }
 
-PageTransitions();
+PageTransitions(); */
